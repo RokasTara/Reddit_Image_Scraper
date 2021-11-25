753b0aa2e5b7144f9a52964be2fa0277258fff36
Rokas Tarasevičius <rokas.tarasevicius@gmail.com>
2021-11-25 11:21:33 +0200

    dowloads images from different subreddits

--- scraper/main.py
+++ scraper/main.py
@@ -1,16 +1,29 @@
 import praw
 import json
+import time
 from utils import *
 
 reddit = create_reddit_object()
 
+subreddits = ["memes", "dankmemes", "meme", "Memes_Of_The_Dank"]
+
 subreddit = reddit.subreddit('memes')
 
 hot = subreddit.hot(limit=1000)
+date = get_current_date()
 
-for post in hot:
-    print(post.title)
-    print(post.url)
-    print("\n")
+for name in subreddits:
+    top = reddit.subreddit(name).top(limit=50, time_filter='day')
+    create_directory(name)
+    path = create_directory_for_date(name, get_current_date())
+    
+    for submission in top:
+        if submission.url.endswith(".jpg") or submission.url.endswith(".png") or submission.url.endswith(".gif") or submission.url.endswith(".jpeg") or submission.url.endswith(".webp"): 
+            filename = generate_filename(submission.url, path)
+            if not os.path.isfile(filename):
+                download_image(submission.url, filename)
+                print("Downloaded: " + submission.url)
+            else:
+                print("Already Downloaded: " + submission.url)
 
-    download_image(post.url, generate_filename(post.url))
+    
--- scraper/utils.py
+++ scraper/utils.py
@@ -1,6 +1,8 @@
 import json
 import praw
+import time
 import urllib.request
+import os
 
 def save_credentials(credentials):
     with open('credentials.json', 'w') as f:
@@ -30,5 +32,31 @@ def download_image(url, filename):
         print("failed to download the file")
         return
     
-def generate_filename(url):
-    return "downloads/" + url.split('/')[-1]
+def generate_filename(url, path):
+    return path + url.split('/')[-1]
+
+# get current date
+
+def get_current_date():
+    return time.strftime("%Y-%m-%d")
+
+# create a directory for the current date with name of subreddit and current date
+def create_directory(subreddit : str): 
+    directory = f"{subreddit}"
+    try: 
+        os.mkdir("downloads/" + directory)
+    except:
+        print(f"Directory \"{directory}\" already exist")
+        pass
+    return directory
+
+def create_directory_for_date(subreddit : str, date : str): 
+    directory = f"downloads/{subreddit}/{date}"
+    try: 
+        os.mkdir(directory)
+    except:
+        print(f"Directory \"{directory}\" already exist")
+        pass
+    return directory
+    
+
